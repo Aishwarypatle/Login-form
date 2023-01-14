e201c69025f7ecc7ddb144322e03f22977d81071
Aishwary Patle <61194375+Aishwarypatle@users.noreply.github.com>
2023-01-14 19:39:36 +0530

    REACT Added

---
+++ myapp/src/components/Form.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Form = () => {
+  return (
+  <>
+    <div className='bg-dark d-flex justify-content-center justify-content-center 50vh 50vh shadow-lg backg'>
+        <div>
+
+        </div>
+    </div>
+  </>
+  )
+}
+
+export default Form
