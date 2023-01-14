7b51dde74167ed00533e2b8cce2342de9d9836ae
Aishwary Patle <61194375+Aishwarypatle@users.noreply.github.com>
2023-01-14 23:33:55 +0530

    completed

--- myapp/src/components/Form.jsx
+++ myapp/src/components/Form.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import {IoPersonCircle} from 'react-icons/io5'
+import {IoPersonSharp} from 'react-icons/io5'
 import {GoKey} from 'react-icons/go'
 import Google from '../images/google.png'
 import Github from '../images/github.png'
@@ -9,30 +9,45 @@ import '../components/form.css'
 const Form = () => {
   return (
   <>
-    <div className='bg-dark d-flex justify-content-center align-items-center 100vh 100vh shadow-lg backg'>
-        <div className='d-flex justify-content-center align-items-center 50vh 50vh'>
-            <div className='d-grid p-auto'>
+    <div className='bg-dark d-flex justify-content-center align-items-center vh-100 vw-100 shadow-lg backg'>
+        <div className='card m-auto d-flex justify-content-center align-items-center'>
+            <div className='d-grid'>
                 <div className='container'>
-                    <div className='header'>
-                        <h3 className='m-auto fw-bold '>Login</h3>                   
+                    <div className='header d-flex justify-content-center'>
+                        <h3 className='m-auto fw-bold mt-4'>Login</h3>                   
+                    </div>
+
+                    <div className='d-grid mt-3'>
+                       <div className='field d-grid'>
+                            <p className='fw-bold'>Username</p>
+                                <div className='d-flex justify-content-center m-0'>
+                                        <IoPersonSharp style={{margin:"auto"}} />
+                                    <input type="text" placeholder="Username"></input>
+                                </div>
+                        </div>
+
+                        <div className='field d-grid'>
+                            <p className='fw-bold mt-3'>Password</p>
+                            <div className='d-flex justify-content-center m-0'>
+                                        <GoKey style={{margin:"auto"}} />
+                                    <input type="password" placeholder="Password"></input>
+                            </div>
+                        </div>
+                        <div className='d-flex justify-content-end mt-2'>
+                            <a href='/'>Forget Password?</a>
+                        </div>
+                        <button className='p-2 mt-4 fw-bold'> SUBMIT</button>
                     </div>
 
                     <div className='d-grid'>
-                        <p>Username</p>
-                        <span> <IoPersonCircle /> </span><input type="text" placeholder="Username"></input>
-                        <p>Password</p>
-                        <span><GoKey /></span> <input type="password" ></input>
-                        <a href='/'>Forgot Password</a>
-                        <button className='btn btn-danger p-2'> Submit</button>
-                    </div>
-
-                    <div className='d-grid'>
-                        <h5 className='m-auto'>Or Sign-Up Using</h5>
-                        <div className='d-flex justify-content-evenly align-item-center'>
+                        <h6 className='m-auto mt-4'>Or Sign-Up Using</h6>
+                        <div className='d-flex justify-content-evenly align-item-center mt-3'>
                             <img src={Google} alt="google"></img>
                             <img src={Github} alt="github"></img>
                             <img src={Twitter} alt="twitter"></img>
-
+                            <br/>
+                            <br/>
+                            
                         </div>
                     </div>
                 </div>
